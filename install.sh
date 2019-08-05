0d63050469d88ff996761be8ddc34443dc264350
Alexandra Marie Miller <accouts@alexandra.systems>
2019-08-04 19:37:30 -0700

    Updated scripts.

--- install.sh
+++ install.sh
@@ -1,1 +1,8 @@
+#/usr/bin/env bash
 
+for scriptBase in `ls *.util.sh`
+do
+    echo "INSTALLING $scriptBase"
+    scriptTarget=`echo $scriptBase | awk -F. '{print $1}'`
+    cp $scriptBase $HOME/bin/$scriptTarget
+done
