b909c2042d93849896304fcbc9db7eccd20c9a32
Alexandra Marie Miller <accounts@alexandra.systems>
2019-08-05 23:21:29 -0700

    updated install script

--- install.sh
+++ install.sh
@@ -1,8 +1,21 @@
 #/usr/bin/env bash
 
-for scriptBase in `ls *.util.sh`
+
+echo ""
+echo "Installing local scripts:"
+for userScriptSource in `ls *.util.sh`
 do
-    echo "INSTALLING $scriptBase"
-    scriptTarget=`echo $scriptBase | awk -F. '{print $1}'`
-    cp $scriptBase $HOME/bin/$scriptTarget
+    echo "Installing: $userScriptSource"
+    userScriptTarget=`echo $userScriptSource | awk -F. '{print $1}'`
+    cp $userScriptSource $HOME/bin/$userScriptTarget
+done
+echo ""
+echo "For security purposes install the following global and root only scripts manually:"
+for globalScriptSource in `ls *.global.util.sh`
+do
+    echo "$globalScriptSource"
+done
+for rootScriptSource in `ls *.root.util.sh`
+do
+    echo "$rootScriptSource"
 done
